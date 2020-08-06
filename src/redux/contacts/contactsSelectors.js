471248b84405bd969470724703f6fbba31a37660
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-06 20:19:36 +0200

    finished all tasks of hw-07

--- src/redux/contacts/contactsSelectors.js
+++ src/redux/contacts/contactsSelectors.js
@@ -1,15 +1,18 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const itemsSelector = (state) => state.contacts.items;
 export const filterSelector = (state) => state.contacts.filter;
 
-export const getFilteredContact = (state) => {
-  const items = itemsSelector(state);
-  const filter = filterSelector(state).toLowerCase();
+export const getFilteredContact = createSelector(
+  [itemsSelector, filterSelector],
+  (items, filter) => {
+    return items.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
 
-  return items.filter((contact) => contact.name.toLowerCase().includes(filter));
-};
-
-export const getContactById = (state, contactId) => {
-  const items = itemsSelector(state);
-
-  return items.find((contact) => contact.id === contactId);
-};
+export const getContactById = createSelector(
+  [itemsSelector, (state, contactId) => contactId],
+  (items, contactId) => items.find((contact) => contact.id === contactId)
+);
